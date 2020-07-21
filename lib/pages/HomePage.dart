aba8ca88346252abcf3851846a6aae39b52f24ca
Masayuki Onishi <omccello@gmail.com>
2020-07-22 00:21:02 +0900

    ダミーjson用意

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -5,6 +5,24 @@ class HomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+final List<Map> dummyItem = [
+  {
+    "id": 1,
+    "name": "つるはし",
+    "item_type": "sword",
+    "ask_price": 240,
+    "sell_price": 100,
+    "description": "ダンジョンの壁を掘れる",
+    "comment": null,
+    "attack_num": 1,
+    "defence_num": null,
+    "テーブルマウンテン": "○",
+    "掛軸裏の洞窟": "○",
+    "食神のほこら": null,
+    "フェイの最終問題": "○"
+  },
+];
+
 class _MyHomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
