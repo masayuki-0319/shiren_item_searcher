d489df205350fba1718bf6f3e1582719d56ab4e5
Masayuki Onishi <omccello@gmail.com>
2020-07-30 00:34:18 +0900

    itemデータ取得を切り出し

--- lib/widgets/ViewItems.dart
+++ lib/widgets/ViewItems.dart
@@ -1,53 +1,10 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+
+import '../services/fetch_item.dart';
 
 final _backgroundColor = Colors.green[100];
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
-final dummyItems = [
-  {
-    "id": 1,
-    "name": "つるはし",
-    "item_type": "sword",
-    "ask_price": 240,
-    "sell_price": 100,
-    "description": "ダンジョンの壁を掘れる",
-    "comment": null,
-    "attack_num": 1,
-    "defence_num": null,
-    "テーブルマウンテン": "○",
-    "掛軸裏の洞窟": "○",
-    "食神のほこら": null,
-    "フェイの最終問題": "○"
-  },
-  {
-    "id": 3,
-    "name": "こん棒",
-    "item_type": "sword",
-    "ask_price": 240,
-    "sell_price": 80,
-    "description": "特になし",
-    "comment": null,
-    "attack_num": 2,
-    "defence_num": null,
-    "テーブルマウンテン": "○",
-    "掛軸裏の洞窟": "○",
-    "食神のほこら": null,
-    "フェイの最終問題": "○"
-  },
-];
-
-Future<String> loadJsonItemList() async {
-  return rootBundle.loadString('assets/data/item_list.json');
-}
-
-Future<List> fetchItemList() async {
-  String jsonData = await loadJsonItemList();
-  List loadData = jsonDecode(jsonData);
-  return (loadData != null) ? loadData : dummyItems;
-}
 
 class ViewItems extends StatelessWidget {
   static List itemList;
